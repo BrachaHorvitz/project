994ae24558d7ad12f3e48f847e79da3fb18b8ee0
Bracha Horvitz <101530967+BrachaHorowitz@users.noreply.github.com>
2023-05-11 19:34:30 +0300

    commit server

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ require('./config/db')
 
 const express = require("express");
 const cors = require("cors");
-const app=express();
+const app = express();
 
-const MemberRouter =require("./routes/memberRoutes")
+const MemberRouter = require("./routes/memberRoutes")
 const CoronaRouter = require("./routes/coronaRoutes")
+const path = require('path');
 
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/member", MemberRouter);
 app.use("/corona", CoronaRouter)
 
-app.listen(5000,()=>{
-    console.log("listening port 5000 :)");
-})
+const PORT = process.env.PORT || 5000; 
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
