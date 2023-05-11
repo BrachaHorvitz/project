4a3083dcc121acfc0e84b0738f2ed01f5f1f6d14
Bracha Horvitz <101530967+BrachaHorowitz@users.noreply.github.com>
2023-05-11 12:02:49 +0300

    commit comments

--- controllers/coronaController.js
+++ controllers/coronaController.js
@@ -3,12 +3,14 @@ const Member = require('../models/memberModel');
 
 exports.createCorona = async (req, res) => {
   try {
+    // Retrieve single Member record by ID from database
     const member = await Member.findOne({ id: req.body.member });
     
+    // if there's no member with the request ID - return error message
     if (!member) {
       return res.status(400).json({message: 'Member not found'});
     }
-    
+
     const coronaData = { ...req.body, member: member._id};
     const corona = await Corona.create(coronaData);
     
@@ -20,7 +22,10 @@ exports.createCorona = async (req, res) => {
 
 exports.getAllCoronas = async (req, res) => {
   try {
+    // Retrieve all Corona records from database
     const coronas = await Corona.find();
+
+    // Return all Corona records
     res.status(200).json(coronas);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -29,13 +34,18 @@ exports.getAllCoronas = async (req, res) => {
 
 exports.getCoronaById = async (req, res) => {
   try {
+    // Retrieve single Corona record by ID from database
     const corona = await Corona.findById(req.params.id);
+
     if (!corona) {
+      // Return error if Corona record is not found
       return res.status(404).json({
         status: 'fail',
         message: 'Corona not found',
       });
     }
+
+    // Return single Corona record
     res.status(200).json(corona);
   } catch (err) {
     res.status(500).json({ message: err.message });
--- controllers/memberController.js
+++ controllers/memberController.js
@@ -2,7 +2,10 @@ const Member = require('../models/memberModel');
 
 exports.getAllMembers = async (req, res) => {
   try {
+    // Retrieve all Members records from database
     const members = await Member.find();
+
+    // Return all Member records
     res.json(members);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -11,7 +14,18 @@ exports.getAllMembers = async (req, res) => {
 
 exports.getMemberById = async (req, res) => {
   try {
+    // Retrieve single Member record by ID from database
     const member = await Member.findById(req.params.id);
+
+    // Return error if Member record is not found
+    if (!member) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Member not found',
+      });
+    }
+
+    // Return single Member record
     res.json(member);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -22,6 +36,8 @@ exports.getMemberById = async (req, res) => {
 exports.createMember = async (req, res) => {
     try {
       const member = await Member.create(req.body);
+
+      // Return new Member record
       res.status(201).json(member);
     } catch (err) {
       res.status(500).json({ message: err.message });
